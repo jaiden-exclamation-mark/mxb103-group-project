07112f20a958898e68286bff3c5c17c531c1aa7a
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2025-10-10 12:04:26 +1000

    Add documentation to unge_kutta().

--- runge_kutta.m
+++ runge_kutta.m
@@ -1,4 +1,14 @@
 function w = runge_kutta(f, a, b, alpha, n)
+% RUNGE_KUTTA  General implementation of the Fourth Order Runge-Kutta method.
+% 
+% Arguments:
+%   f - The derivative d/dt.
+%   a - The start value of t.
+%   b - The end value of t.
+%   alpha - The value of f(a).
+%   n - The number of steps to do.
+% Returns:
+%   w - The approximate value of f(b).
     t = a;
     w = alpha;
     h = (b - a) / n;
