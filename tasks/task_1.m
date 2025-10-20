f56d628f14ff3a036251782b2ee8d88b03c0eca6
harmankaur16 <harman16deep@gmail.com>
2025-10-20 15:35:42 +1000

    Update task_1.m
    Incomplete, have started on task 1

--- tasks/task_1.m
+++ tasks/task_1.m
@@ -1,2 +1,49 @@
 % Include function files in parent directory.
 addpath('..');
+
+clc; clear; close all;
+
+% Parameters as given in task sheet
+H = 74;     % height of jump point
+D = 31;     % deck height
+c = 0.9;    % drag coefficient
+m = 80;     % mass of jumper
+L = 25;     % length of bungee rope
+k = 90;     % spring constant of bungee rope
+g = 9.8;    % gravitational acceleration
+
+C = c / m;  % drag per unit mass with units 1/m
+K = k / m;  % stiffness per unit mass with units 1/s^2
+
+final_T = 60;
+h = 1e-3;
+t = 0 : h : final+T;
+N = length(t);
+
+y0 = 0;
+v0 = 0;z
+w = zeros(2, N);
+w(:,1) = [y0, v0];
+
+for i = 2:N
+    k1 = h * rhs_bungee(w(:, i - 1), g, C, K, L);
+    k2 = h * rhs_bungee(w(:, i - 1) + 0.5*k1, g, C, K, L);
+    k3 = h * rhs_bungee(w(:, i - 1) + 0.5*k2, g, C, K, L);
+    k4 = h * rhs_bungee(w(:, i - 1) + k3, g, C, K, L);
+    w(:, i) = w(:, i - 1) + (k1 + 2*k2 + 2*k3 + k4) / 6;
+end
+
+y = w(1, :);
+v = w(2, :);
+
+% Plot for y(t)
+figure;
+plot(t, y, 'LineWidth', 1.2);
+grid on;
+hold on;
+xlabel('Time (s)');
+ylabel('Position (m)');
+title('Bungee jump position vs time')
+
+
+% I've started this section, but haven't fully finished it yet.
