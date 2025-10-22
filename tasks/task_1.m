d71590f80364216f6cde7710c16fffd27b4e8b68
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2025-10-22 23:04:38 +1000

    Add comment on currying `rhs_bungee()`.

--- tasks/task_1.m
+++ tasks/task_1.m
@@ -31,6 +31,7 @@ end
 y_0 = 0;
 v_0 = 0;
 
+% Curry `rhs_bungee` so then g, C, K, L are essentially "baked in"to the function.
 f = @(t, w) rhs_bungee(w, g, C, K, L);
 [t, w] = runge_kutta(f, 0, 60, [y_0; v_0], 60 / 1e-3);
 y = w(1, :);
