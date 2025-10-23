89589c675dd501fe51dfcd9071a926ad420e99ab
harmankaur16 <harman16deep@gmail.com>
2025-10-24 00:55:04 +1000

    Update task_1.m
    Updated the code as it was showing an error when i tried to run it before. Also added code to estimate the time the 10th bounce occurred and added markers on the plot to show the lowest points (bounces).

--- tasks/task_1.m
+++ tasks/task_1.m
@@ -3,8 +3,9 @@ addpath('..');
 clc; clear; close all;
 
 % Initialise
+final_T = 60;
 init_parameters;
-[t, y, ~] = calculate_y_and_v(60 / 1e-3, g, C, K, L);
+[t, y, ~] = calculate_y_and_v(final_T / 1e-3, g, C, K, L);
 
 % Plot for y(t)
 figure;
@@ -15,15 +16,25 @@ xlabel('Time (s)');
 ylabel('Position (m)');
 title('Bungee jump position vs time')
 
-% Automatically count the number of bounces
-
 % Detect minima in y(t) to count bounces
-[min_peaks, min_locs] = findpeaks(-y, t); % find negative peaks (lowest points)
-num_bounces = length(min_peaks);
+approx_velocity = gradient(y, t);
+low_points = find(approx_velocity(1:end-1) < 0 & approx_velocity(2:end) > 0); %finds negative peaks
+num_bounces = length(low_points);
 fprintf('Number of bounces in %.1f seconds: %d\n', final_T, num_bounces);
 
 if num_bounces >= 10
-    fprintf('The 10th bounce occurs at t = %.2f seconds.\n', min_locs(10));
+    fprintf('The 10th bounce occurs at t = %.2f seconds.\n', t(low_points(10)));
 else
-    fprintf('Only %d bounces occurred within %.1f seconds .\n', num_bounces, final_T);
+    fprintf('Only %d bounces occurred within %.1f seconds.\n', num_bounces, final_T);
+    
+    % Estimate time of 10th bounce if after 10 seconds by using average
+    % period between last two bounces.
+    if length(low_points) > 2 
+        gap = (t(low_points(end)) - t(low_points(end-1)));
+        estimate = t(low_points(end)) + gap;
+        fprintf('Estimated 10th bounce at approximately %.2f seconds. \n', estimate);
+    end
+    
 end
+% Marks the lowest points (bounces)
+plot(t(low_points), y(low_points), 'r*');
