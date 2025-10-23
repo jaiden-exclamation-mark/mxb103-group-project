7561551b936c25b18b2997d588107679137dffb9
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2025-10-23 13:11:09 +1000

    Redo entirety of `task_5.m`

--- tasks/task_5.m
+++ tasks/task_5.m
@@ -2,38 +2,22 @@
 addpath('..');
 clear; close all;
 
-task_4
+% Initialise
+init_parameters;
+number_of_points = 1000;
 
-function updated = push(value, buffer)
-% PUSH  Pushes a value onto the end of a buffer and removes the first element.
-%
-% Arguments:
-%   value - The value to push to the end of the buffer.
-%   buffer - An array meant to act like a fixed-length buffer.
-% Returns:
-%   updated - An array with all of the contents of `buffer` from the second element to the end, with `value` as the last element.
-    updated = buffer;
-    len = length(buffer);
-    updated(1:len - 1) = updated(2:len);
-    updated(len) = value;
+[t, y, ~] = calculate_y_and_v(number_of_points, g, C, K, L);
+
+camera_height = H - D;
+for i = 1:length(y) - 3
+    interpolating_y = y(i:i + 3);
+    
+    % Since y_{i+3} > H-D, break on first value of interpolating_y(3) > H-D
+    if interpolating_y(3) > camera_height
+        interpolating_t = t(i:i + 3);
+        break;
+    end
 end
 
-% The below function exists because it's simpler than MATLAB's `struct` syntax.
-function p = point(x, y)
-% POINT  Creates a struct for a Cartesian point.
-%
-% Arguments:
-%   x - The x component of the point.
-%   y - The y component of the point.
-% Returns:
-%   p - A struct with fields `x` and `y`.
-    p.x = x;
-    p.y = y;
-end
-
-
-origin = point(0, 0);
-% Task states that four points around H-D must be selected, so create buffer of length 4.
-points_buffer = [origin, origin, origin, origin];
-
-% TODO: use task_4's numerical integration to get a set of points to interpolate for the question.
+interpolating_t
+interpolating_y
