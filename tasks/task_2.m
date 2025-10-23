a15697331c895fd1855f73aeecd09ad1d12a4027
harmankaur16 <harman16deep@gmail.com>
2025-10-24 01:25:38 +1000

    Update task_2.m
    Added code to find and display the jumper’s maximum speed and the time it occurs. Uses the absolute velocity to determine the fastest point, marks it with a red star on the velocity-time graph, and also prints the speed and time in the Command Window.

--- tasks/task_2.m
+++ tasks/task_2.m
@@ -1,5 +1,13 @@
 % Include function files in parent directory.
 addpath('..');
+clc; clear; close all;
+
+init_parameters;
+y0 = 0; v0 =0;
+final_T = 60; h = 1e-3;
+
+f = @(t,w) rhs_bungee(w, g, C, K, L);
+[t, w] = runge_kutta(f, 0, final_T, [y0; v0], final_T/h);
 
 % Extract velocity from RK4 results
 v = w(2, :);
@@ -12,6 +20,15 @@ xlabel('Time (s)');
 ylabel('Velocity (m/s)');
 title('Bungee Jumper Velocity vs Time');
 
-% I did part of this question, i showed the displaying of velocity-time graph
-% and see when the max speed occurs visually. Need to show how fast the jumper
-%goes at their fastest point and also when it happens
+% Find and display the maximum speed of jumper and when it occurs
+speed = abs(v);
+max_speed = max(speed);
+time_of_max_speed = t(speed == max_speed);
+
+% Mark maximum speed point on graph
+hold on
+plot(time_of_max_speed, v(speed == max_speed), 'r*')
+
+% Display results in command window
+fprintf('Maximum speed: %.2f m/s at time: %.2f seconds. \n', max_speed, time_of_max_speed);
+
