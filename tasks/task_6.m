7c30c04f815178a6d24ce1cb43577eb54e54263f
jaiden-exclamation-mark <jaidenthomas271@outlook.com>
2025-10-25 13:48:49 +1000

    Start adapting Damien's work for Task 6

--- tasks/task_6.m
+++ tasks/task_6.m
@@ -1,2 +1,23 @@
 % Include function files in parent directory.
 addpath('..');
+clear; close all;
+
+% Initialise
+init_parameters;
+
+% Alter length of bungee rope `L`
+for L = 40:45
+    % Alter spring constant `k` of rope.
+    for k = 75:85
+        [t, y, v] = calculate_y_and_v(60 / 1e-3, g, C, k / m, L);
+        % Use first-order difference approximation for acceleration.
+        a = diff(v) ./ diff(t);
+
+        if max(a) > 2 * g
+            fprintf('Model with parameters L = %d and k = %d rejected due to unsafe acceleration.', L, k);
+            continue;
+        end
+
+        
+    end
+end
